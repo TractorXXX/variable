2beb091397a4a2f642e33f3ef036a39a3c613a6a
Константин <tractorx@yandex.ru>
2024-08-31 14:29:48 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,6 @@
+homework_count = 12
+expended_hours_count = 1.5
+course_name = 'Python'
+time_of_homework = expended_hours_count / homework_count
+print('Курс:',course_name, ', всего задач:', homework_count, ', затрачено часов:', expended_hours_count, ', среднее время выполнения', time_of_homework, 'часа')
+print('Курс:', str(course_name) + ',', 'всего задач:', str(homework_count) + ',', 'затрачено часов:', str(expended_hours_count) + ',', 'среднее время выполнения', time_of_homework, 'часа.')
